23cf0c84e9d2734ac6673628d3fced109cedda68
MinhNguyenETH <minhnguyen.eth@gmail.com>
2025-04-17 14:28:03 +0700

    Update BasePage

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,8 +1,7 @@
 package base;
 
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,13 +11,20 @@ import java.time.Duration;
 public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected Actions actions;
+    protected JavascriptExecutor jsExecutor;
 
-    // Constructor để khởi tạo driver và wait
+    // Constructor để khởi tạo driver, wait, actions, jsExecutor
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        this.actions = new Actions(driver);
+        this.jsExecutor = (JavascriptExecutor) driver;
         PageFactory.initElements(driver, this);
     }
+
+    // ==== Selenium thường ====
+
     public void sendKeys(WebElement element, String value){
         wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(value);
     }
@@ -35,19 +41,43 @@ public class BasePage {
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 
-
-    // Veriry url
-    public boolean verifyUrl(String url)
-    {
-        System.out.println(driver.getCurrentUrl());
-        System.out.println(url);
-
-        return driver.getCurrentUrl().contains(url); // True, False
+    public boolean verifyUrl(String url) {
+        return driver.getCurrentUrl().contains(url);
     }
 
-    // Kiểm tra xem phần tử có hiển thị hay không
     public boolean isElementVisible(WebElement element) {
         return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
     }
 
+    // ==== JavaScriptExecutor ====
+
+    public void scrollToElement(WebElement element) {
+        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void clickElementByJS(WebElement element) {
+        jsExecutor.executeScript("arguments[0].click();", element);
+    }
+
+    public void setInputValueByJS(WebElement element, String value) {
+        jsExecutor.executeScript("arguments[0].value='" + value + "';", element);
+    }
+
+    // ==== Actions ====
+
+    public void hoverToElement(WebElement element) {
+        actions.moveToElement(element).perform();
+    }
+
+    public void dragAndDrop(WebElement source, WebElement target) {
+        actions.dragAndDrop(source, target).perform();
+    }
+
+    public void clickAndHold(WebElement element) {
+        actions.clickAndHold(element).perform();
+    }
+
+    public void doubleClick(WebElement element) {
+        actions.doubleClick(element).perform();
+    }
 }
